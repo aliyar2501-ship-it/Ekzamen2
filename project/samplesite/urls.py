c63067d570a23fc378dad391c26bd994fe978b25
FrAv2501 <aliyar2501@gmail.com>
2026-07-07 21:26:58 +0500

    Update urls.py

--- project/samplesite/urls.py
+++ project/samplesite/urls.py
@@ -19,6 +19,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('board/', include('board.urls')),
+    path('', include('board.urls')),
     path('accounts/', include('django.contrib.auth.urls'))
 ]
